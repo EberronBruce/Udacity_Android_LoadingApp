3f0bf059f7c97e127add24c37a3d026e443fb23e
Bruce <bruce@redravencomputing.com>
2021-09-08 16:21:51 -0600

    Added circle animation

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,16 +2,16 @@
 
 package com.udacity
 
+import android.animation.Animator
 import android.animation.AnimatorInflater
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import androidx.core.animation.addListener
 import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
@@ -20,26 +20,48 @@ private const val TAG = "LoadingButton"
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    
+
     private var bgColor: Int = Color.BLACK
     private var textColor: Int = Color.BLACK // default color
 
     private var widthSize = 0
     private var heightSize = 0
 
+    private var currentDegree = 0f
+    private val r = Rect()
+    val rectF = RectF()
+
     // tells the compiler that the value of a variable
     // must never be cached as its value may change outside
     @Volatile
     private var progress: Double = 0.0
     private var valueAnimator: ValueAnimator
+    private var circleAnimator: ValueAnimator
 
-    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { _, _, _ ->
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { p, old, new ->
+
+        when(new) {
+            ButtonState.Loading -> {
+                Log.d(TAG, "Button State Loading")
+            }
+            ButtonState.Completed -> {
+                Log.d(TAG, "Button State Complete")
+            }
+            ButtonState.Clicked -> {
+                Log.d(TAG, "Button State Clicked")
+            }
+        }
     }
 
     private val updateListener = ValueAnimator.AnimatorUpdateListener {
         progress = (it.animatedValue as Float).toDouble()
         invalidate() //redraw the screen
         requestLayout() //when rectangular progress dimension changes
+    }
+
+    private val updateCircleListener = ValueAnimator.AnimatorUpdateListener {
+        currentDegree = it.animatedValue as Float
+        invalidate()
     }
 
     // call after downloading is completed
@@ -56,8 +78,15 @@ class LoadingButton @JvmOverloads constructor(
         isClickable = true
         valueAnimator =
             AnimatorInflater.loadAnimator(context, R.animator.loading_animator) as ValueAnimator
-
         valueAnimator.addUpdateListener(updateListener)
+        valueAnimator.repeatCount = ValueAnimator.INFINITE
+        valueAnimator.repeatMode = ValueAnimator.RESTART
+
+        circleAnimator =
+            AnimatorInflater.loadAnimator(context, R.animator.circle_animator) as ValueAnimator
+        circleAnimator.addUpdateListener(updateCircleListener)
+        circleAnimator.repeatCount = ValueAnimator.INFINITE
+        circleAnimator.repeatMode = ValueAnimator.RESTART
 
         // initialize custom attributes of the button
         val attr = context.theme.obtainStyledAttributes(
@@ -99,6 +128,7 @@ class LoadingButton @JvmOverloads constructor(
     // start the animation when button is clicked
     private fun animation() {
         valueAnimator.start()
+        circleAnimator.start()
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -112,14 +142,8 @@ class LoadingButton @JvmOverloads constructor(
         if (buttonState == ButtonState.Loading) {
             paint.color = ContextCompat.getColor(context, R.color.colorAccent)
             canvas?.drawRect(0f, 0f, (width * (progress / 100)).toFloat(), height.toFloat(), paint)
-            Log.d(TAG, "progress : $progress")
-            Log.d(TAG, "buttonState : $buttonState")
-            if(progress >= 100.0) {
-                progress = 0.0
-                invalidate()
-                animation()
-            }
         }
+
         // check the button state
         val buttonText = if (buttonState == ButtonState.Loading)
             resources.getString(R.string.button_loading)
@@ -128,6 +152,44 @@ class LoadingButton @JvmOverloads constructor(
         // write the text on custom button
         paint.color = textColor
         canvas?.drawText(buttonText, (width /2).toFloat(), ((height + 30) / 2).toFloat(), paint)
+
+        canvas?.getClipBounds(r)
+        val cHeight = r.height()
+        val cWidth = r.width()
+
+        paint.getTextBounds(buttonText, 0, buttonText.length, r)
+
+
+        rectF.set(
+            (cWidth / 2f + r.width() * 0.65f) - 25f,
+            (cHeight / 2) - 25f,
+            (cWidth / 2f + r.width() * 0.65f) + 25f,
+            (cHeight / 2) + 25f
+        )
+
+        paint.color = Color.WHITE
+
+        //Log.d(TAG, "currentDegree : $currentDegree")
+
+        canvas?.drawArc(
+            rectF,
+            0f,
+            currentDegree,
+            true,
+            paint
+        )
+
+//        canvas?.drawArc(
+//            width.toFloat()  * 0.8F - 25F,
+//            height.toFloat() / 2 - 25F,
+//            width.toFloat()  * 0.8F + 25F,
+//            height.toFloat() / 2 + 25F,
+//            0.0f,
+//            currentDegree,
+//            true,
+//            paint)
+
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {

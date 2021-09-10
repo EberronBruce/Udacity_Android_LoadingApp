02bda3474124081d2bce41a1456fc5e86629fcad
Bruce <bruce@redravencomputing.com>
2021-09-09 23:10:22 -0600

    Clean up code for main and notifications

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -3,11 +3,11 @@ package com.udacity
 import android.app.DownloadManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.Cursor
 import android.graphics.Color
 import android.net.Uri
 import android.os.Build
@@ -17,10 +17,10 @@ import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.udacity.databinding.ActivityMainBinding
+import com.udacity.notification.sendNotification
 
 private const val TAG = "Main_Activity_Tag"
 
@@ -29,8 +29,6 @@ class MainActivity : AppCompatActivity() {
     private var downloadID: Long = 0
 
     private lateinit var notificationManager: NotificationManager
-    //private lateinit var pendingIntent: PendingIntent
-   // private lateinit var action: NotificationCompat.Action
 
     private lateinit var binding: ActivityMainBinding
 
@@ -42,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         binding.contentMain.customButton.setOnClickListener {
-            //download(UDACITY_URL)
             val loadingButton = it as LoadingButton
             loadingButton.changeButtonState(ButtonState.Loading)
             setupCustomButtonOnClickLister(loadingButton)
@@ -72,17 +69,10 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-            //val action = intent?.action
             val downloadManager : DownloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-
             Log.d(TAG, "id : $id")
             if (id == downloadID) {
                 createBroadcastReceiver(context, downloadManager)
-//                Log.d(TAG, "Finish Download")
-//                //Give some delay to see animation
-//                Handler(Looper.getMainLooper()).postDelayed({
-//                    binding.contentMain.customButton.changeButtonState(ButtonState.Completed)
-//                }, 1000)
             }
         }
     }
@@ -96,34 +86,40 @@ class MainActivity : AppCompatActivity() {
             var column = cursor.getColumnIndex(DownloadManager.COLUMN_TITLE)
             val title = if (column >= 0) cursor.getString(column) else "No Title"
             column = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
-            when (cursor.getInt(column)) {
-                DownloadManager.STATUS_SUCCESSFUL -> {
-                    Log.d(TAG, "Status Success : ${DownloadManager.STATUS_SUCCESSFUL}")
+            cursor.downloadManagerSelection(context, column, title)
+        }
+    }
+
+    private fun Cursor.downloadManagerSelection(context: Context?, column: Int, title: String) {
+        when (getInt(column)) {
+            DownloadManager.STATUS_SUCCESSFUL -> {
+                Log.d(TAG, "Status Success : ${DownloadManager.STATUS_SUCCESSFUL}")
+                //Give some delay to see animation
+                Handler(Looper.getMainLooper()).postDelayed({
+                    Toast.makeText(context, "Download Success", Toast.LENGTH_SHORT).show()
                     binding.contentMain.customButton.changeButtonState(ButtonState.Completed)
                     sendCompleteNotify(title, DownloadManager.STATUS_SUCCESSFUL)
-                }
-                DownloadManager.STATUS_FAILED -> {
-                    Log.d(TAG, "Status Failed : ${DownloadManager.STATUS_FAILED}")
-                    Toast.makeText(context, "Failed to download", Toast.LENGTH_SHORT).show()
-                    binding.contentMain.customButton.changeButtonState(ButtonState.Completed)
-                    sendCompleteNotify(title, DownloadManager.STATUS_FAILED)
-                }
-                DownloadManager.STATUS_PAUSED -> {
-                    Log.d(TAG, "Status Paused : ${DownloadManager.STATUS_PAUSED}")
-                }
-                DownloadManager.STATUS_PENDING -> {
-                    Log.d(TAG, "Status Pending : ${DownloadManager.STATUS_PENDING}")
-                }
-                DownloadManager.STATUS_RUNNING -> {
-                   Log.d(TAG, "Status Running : ${DownloadManager.STATUS_RUNNING}")
-                }
-                else -> {
+                }, 1000)
 
-                }
             }
+            DownloadManager.STATUS_FAILED -> {
+                Log.d(TAG, "Status Failed : ${DownloadManager.STATUS_FAILED}")
+                Toast.makeText(context, "Download Failed", Toast.LENGTH_SHORT).show()
+                binding.contentMain.customButton.changeButtonState(ButtonState.Completed)
+                sendCompleteNotify(title, DownloadManager.STATUS_FAILED)
+            }
+            DownloadManager.STATUS_PAUSED -> {
+                Log.d(TAG, "Status Paused : ${DownloadManager.STATUS_PAUSED}")
+            }
+            DownloadManager.STATUS_PENDING -> {
+                Log.d(TAG, "Status Pending : ${DownloadManager.STATUS_PENDING}")
+            }
+            DownloadManager.STATUS_RUNNING -> {
+                Log.d(TAG, "Status Running : ${DownloadManager.STATUS_RUNNING}")
+            }
+            else -> {
 
-
-
+            }
         }
     }
 
@@ -139,7 +135,6 @@ class MainActivity : AppCompatActivity() {
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
-        Log.d(TAG, "downloadID : $downloadID")
     }
 
     companion object {
@@ -147,8 +142,6 @@ class MainActivity : AppCompatActivity() {
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val GLIDE_URL = "https://github.com/bumptech/glide"
         private const val RETROFIT_URL = "https://github.com/square/retrofit"
-
-        //private const val CHANNEL_ID = "channelId"
     }
 
     private fun createChannel(id : String, name: String) {
@@ -166,30 +159,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendCompleteNotify(title: String, status: Int) {
         notificationManager = ContextCompat.getSystemService(application, NotificationManager::class.java) as NotificationManager
+        notificationManager.sendNotification(application.getString(R.string.notification_description), applicationContext, title, status)
+    }
 
-        val contentIntent = Intent(applicationContext, DetailActivity::class.java)
-        contentIntent.putExtra("download-title", "$title")
-        contentIntent.putExtra("download-status", "$status")
-        val contentPendingIntent = PendingIntent.getActivities(
-            applicationContext,
-            0,
-            arrayOf(contentIntent),
-            PendingIntent.FLAG_IMMUTABLE
-        )
-
-        val builder = NotificationCompat.Builder(
-            applicationContext,
-            applicationContext.getString(R.string.notifyChannelId)
-        )
-            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
-            .setContentTitle(applicationContext.getString(R.string.notification_title))
-            .setContentText("TESTING")
-            .setContentIntent(contentPendingIntent)
-            .addAction(R.drawable.ic_assistant_black_24dp, "Check Download", contentPendingIntent )
-            .setAutoCancel(true)
-
-        notificationManager.notify(0, builder.build())
-
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
 }
--- app/src/main/java/com/udacity/notification/NotificationHelper.kt
+++ app/src/main/java/com/udacity/notification/NotificationHelper.kt
@@ -2,10 +2,41 @@ package com.udacity.notification
 
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.udacity.DetailActivity
+import com.udacity.R
 
 
 private const val ID = 0
+private const val NOTIFY_BUTTON_TITLE = "Check Download"
+private const val KEY_DOWNLOAD_TITLE = "download_title"
+private const val KEY_DOWNLOAD_STATUS = "download_status"
 
-fun NotificationManager.sendNotification() {
+fun NotificationManager.sendNotification(message: String, applicationContext: Context, title: String, status: Int) {
 
+    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+    contentIntent.putExtra(KEY_DOWNLOAD_TITLE , title)
+    contentIntent.putExtra(KEY_DOWNLOAD_STATUS, status)
+    val contentPendingIntent = PendingIntent.getActivities(
+        applicationContext,
+        ID,
+        arrayOf(contentIntent),
+        PendingIntent.FLAG_IMMUTABLE
+    )
+
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.notifyChannelId)
+    )
+        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(message)
+        .setContentIntent(contentPendingIntent)
+        .addAction(R.drawable.ic_assistant_black_24dp, NOTIFY_BUTTON_TITLE, contentPendingIntent )
+        .setAutoCancel(true)
+
+    notify(0, builder.build())
 }
